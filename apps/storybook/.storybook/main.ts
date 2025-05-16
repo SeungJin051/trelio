ec16f51124d622b7f5b53c081631d8c161b47a69
임승진 <seungjin051@gmail.com>
2025-05-17 03:31:41 +0900

    chore: storybook 초기 설정 변경 및 기본 예제 파일 제거

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -11,11 +11,29 @@ const config: StorybookConfig = {
     '@storybook/addon-onboarding',
     '@storybook/addon-actions',
     '@storybook/addon-viewport',
+    '@storybook/addon-docs',
+    '@storybook/addon-backgrounds',
+    '@storybook/addon-controls',
+    '@storybook/addon-outline',
+    '@storybook/addon-measure',
     '@chromatic-com/storybook',
   ],
   framework: {
     name: '@storybook/nextjs',
     options: {},
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+  staticDirs: ['../public'],
+  managerHead: (head) => {
+    return `
+      ${head}
+      <script>
+        // GitHub Pages에서 올바른 경로를 사용하기 위한 설정
+        window.STORYBOOK_REACT_APP_BASE_PATH = '/pack-and-go/';
+      </script>
+    `;
   },
 };
 export default config;
