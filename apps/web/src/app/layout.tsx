c2fc326205f2095a567fb95c14cdf45be49f6717
임승진 <seungjin051@gmail.com>
2025-08-06 01:58:51 +0900

    fix: 환경변수 명명 수정

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -27,7 +27,7 @@ export const metadata: Metadata = {
     address: false,
     telephone: false,
   },
-  metadataBase: new URL(process.env.NEXT_PUBLIC_APP_URL as string),
+  metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL as string),
   openGraph: {
     type: 'website',
     locale: 'ko_KR',
